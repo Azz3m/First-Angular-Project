4646fba5063e478fa60740ad533d750fdb0c1469
Eng-Azzam <Azzamazezali@gmail.com>
2019-08-23 13:41:14 +0300

    template of angular

---
+++ app.js
@@ -0,0 +1,12 @@
+//IIFE an immediately invoked function expression
+(function(){
+  'use restrict';
+  //let's define an angular object passing the name and depedentcies
+  angular.module('NameClacASCII',[])
+  //let's define the controller
+  .controller("NameClacASCIIContoller",function(){
+    
+
+  })
+}
+)();
