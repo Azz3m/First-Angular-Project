ce9f2c55837bcde7d0d609185d2199805dd2341e
Eng-Azzam <Azzamazezali@gmail.com>
2019-08-23 13:53:52 +0300

    Finished Project

--- app.js
+++ app.js
@@ -4,8 +4,21 @@
   //let's define an angular object passing the name and depedentcies
   angular.module('NameClacASCII',[])
   //let's define the controller
-  .controller("NameClacASCIIContoller",function(){
-    
+  .controller("NameClacASCIIContoller",function($scope){
+    $scope.name = "Azzam Azez Ali";
+    $scope.valueInNumeric=0;
+    $scope.displayNumeric = function(){
+      var totalValueOfTheName = calculateNameFromString($scope.name);
+      $scope.valueInNumeric=totalValueOfTheName;
+    };
+    function calculateNameFromString(string){
+      var totalStringValue = 0;
+      for(var i=0;i<string.length;i++){
+        totalStringValue += string.charCodeAt(i);
+
+      }
+      return totalStringValue;
+    }
 
   })
 }
